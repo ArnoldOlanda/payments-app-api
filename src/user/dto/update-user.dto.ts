ea0f134c2411a564ae1d91d165bcd5d2e2a16c13
ArnoldOlanda <olanda188@gmail.com>
2026-07-23 22:30:59 -0500

    feat(user): add password validation to UpdateUserDto

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,18 @@
 import { PartialType } from '@nestjs/mapped-types';
+import {
+  IsOptional,
+  IsString,
+  MinLength,
+  ValidateIf,
+} from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsOptional()
+  @ValidateIf(
+    (o) => typeof o.password === 'string' && o.password.length > 0,
+  )
+  @IsString()
+  @MinLength(8)
+  password?: string;
+}
